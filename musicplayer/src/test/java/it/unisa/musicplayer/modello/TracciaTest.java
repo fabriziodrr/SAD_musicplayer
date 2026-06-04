b9a44bd40953eb16351793133921bc186c8d0473
fabriziodrr <fabderrgithub@gmail.com>
2026-06-04 21:44:02 +0200

    Creazione classe di test per il task 12.1. Il sistema si comporta già in questo modo.

--- musicplayer/src/test/java/it/unisa/musicplayer/modello/TracciaTest.java
+++ musicplayer/src/test/java/it/unisa/musicplayer/modello/TracciaTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import it.unisa.musicplayer.modello.Traccia;
-import it.unisa.musicplayer.modello.Tag;
 
 import java.util.Set;
 
