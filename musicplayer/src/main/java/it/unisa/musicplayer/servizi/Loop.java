88331928092efc93a73ca0a2e1e1ff913a14b4b3
antespo9 <atb7799@gmail.com>
2026-06-06 18:49:51 +0200

    US-10: implementate modalità di riproduzione playlist

--- musicplayer/src/main/java/it/unisa/musicplayer/servizi/Loop.java
+++ musicplayer/src/main/java/it/unisa/musicplayer/servizi/Loop.java
@@ -8,7 +8,36 @@ public class Loop extends ModalitaRiproduzione {
 
     @Override
     public Traccia prossimaTraccia(List<Traccia> tracce, Traccia corrente) {
-        // TODO: implementare in una sprint futura
-        return corrente;
+        if (tracce == null || tracce.isEmpty()) {
+            return null;
+        }
+
+        /*
+         * Se la riproduzione non è ancora iniziata,
+         * si parte dalla prima traccia.
+         */
+        if (corrente == null) {
+            return tracce.get(0);
+        }
+
+        int indiceCorrente = tracce.indexOf(corrente);
+
+        /*
+         * Se la traccia corrente non appartiene alla lista,
+         * non è possibile determinare la successiva.
+         */
+        if (indiceCorrente == -1) {
+            return null;
+        }
+
+        /*
+         * Se siamo arrivati all'ultima traccia,
+         * il loop riparte dalla prima.
+         */
+        if (indiceCorrente == tracce.size() - 1) {
+            return tracce.get(0);
+        }
+
+        return tracce.get(indiceCorrente + 1);
     }
 }
